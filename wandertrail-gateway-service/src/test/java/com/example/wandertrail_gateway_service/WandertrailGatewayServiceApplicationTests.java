5ba3dbe0dec963372b11a2d3fb426723c92203e3
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2025-11-14 15:51:13 +0700

    chore: initialize gateway service

---
+++ wandertrail-gateway-service/src/test/java/com/example/wandertrail_gateway_service/WandertrailGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wandertrail_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WandertrailGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

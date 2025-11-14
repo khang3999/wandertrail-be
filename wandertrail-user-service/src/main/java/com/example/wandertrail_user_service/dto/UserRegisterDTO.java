c18b4d1c786c52518b1cca888b35a24c47895734
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2025-11-14 15:33:14 +0700

    chore: config cors security and test fetch api from frontend

---
+++ wandertrail-user-service/src/main/java/com/example/wandertrail_user_service/dto/UserRegisterDTO.java
@@ -0,0 +1,29 @@
+package com.example.wandertrail_user_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+public class UserRegisterDTO {
+    @NotBlank(message = "Username is required")
+    @Size(min = 1, max = 30, message = "Username must be between 1 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 10, message = "Password must be between 6 and 10 characters")
+    private String password;
+
+    @NotBlank(message = "Confirm Password is required")
+    @Size(min = 6, max = 10, message = "Confirm Password must be between 6 and 10 characters")
+    private String confirmPassword;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
